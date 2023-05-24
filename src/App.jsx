ee518be40610ebcefcf6e049cf0fb8f149847686
yorch <jorgellave46@gmail.com>
2023-05-23 23:16:44 -0400

    falta arreglar el dibujo y colocado de imagen

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/views/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Login/>
     </>
   )
 }
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import "./login.css"
-
+import React from "react";
+import "./Login.css";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="login-text-container">
+          <h1>Login</h1>
+        </div>
+        <div className="data-container">
+          <input type="text" id="user-name" className="username" value={"Username:"} />
+          <input type="text" id="pass-word" className="password"value={"Password:"} />
+        </div>
+        <div className="login-button">
+          <button>Login</button>
+        </div>
+      </div>
+      <div className="draw-container">
+        <img src="./src/assets/images/HamburguesaLogin.jpg" alt="Hamburguesa" />
+        <img src="./src/assets/images/HamburguesaLogin.jpg" alt="Hamburguesa" />
+        <img src="./src/assets/images/HamburguesaLogin.jpg" alt="Hamburguesa" />
+        <div className="draw"></div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
