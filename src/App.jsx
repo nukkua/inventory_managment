566f06196fb60468b6167e16a2b0f8082cb26cf3
yerk00 <yibanezs18@gmail.com>
2023-05-27 21:48:29 -0400

    tablap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import TablaPrecio from './Components/TablaPrecio'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1></h1>
+      <TablaPrecio />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+/* import React from 'react'
+import MaterialTable from '@mui/material'
+import './App.css'
+const datos = [
+  { id: 1, nombre: 'Juan', edad: 25 },
+  { id: 2, nombre: 'María', edad: 30 },
+  { id: 3, nombre: 'Pedro', edad: 28 },
+  { id: 4, nombre: 'Laura', edad: 35 },
+];
+
+function App() {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Edad</th>
+        </tr>
+      </thead>
+      <tbody>
+        {datos.map((dato) => (
+          <tr key={dato.id}>
+            <td>{dato.id}</td>
+            <td>{dato.nombre}</td>
+            <td>{dato.edad}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+  
+}
+
+export default App */
