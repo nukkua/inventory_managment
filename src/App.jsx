b1e18c081ac97d442bd59d84b0adac7dee213e22
nukkua <marco.salazar.2014.com@gmail.com>
2023-06-06 12:09:56 -0400

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,45 +7,11 @@ import TablaPrecio from './components/TablaPrecio'
 function App() {
   return (
     <>
-      <Login/>
+      
     </>
   )
 }
 
 export default App;
 
-/* import React from 'react'
-import MaterialTable from '@mui/material'
-import './App.css'
-const datos = [
-  { id: 1, nombre: 'Juan', edad: 25 },
-  { id: 2, nombre: 'María', edad: 30 },
-  { id: 3, nombre: 'Pedro', edad: 28 },
-  { id: 4, nombre: 'Laura', edad: 35 },
-];
 
-function App() {
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th>Edad</th>
-        </tr>
-      </thead>
-      <tbody>
-        {datos.map((dato) => (
-          <tr key={dato.id}>
-            <td>{dato.id}</td>
-            <td>{dato.nombre}</td>
-            <td>{dato.edad}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-  
-}
-
-export default App */
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Login } from './components/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route/>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
