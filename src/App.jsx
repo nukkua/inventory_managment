38d7330e36256102a70592a4107c4e757d05d2d7
nukkua <marco.salazar.2014.com@gmail.com>
2023-06-07 08:02:48 -0400

    hermano es un 1 seg

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <div className="app-container">
         {location.pathname !== "/login" && <NavBar />}
+        
 
         <Routes>
           <Route path="/login" element={<Login />} />
