f9445d26a2bed1c6abffacf80073a44ca1f2183c
nukkua <marco.salazar.2014.com@gmail.com>
2023-06-06 20:22:18 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './components/Login'
-=======
 import { useState } from "react";
 import "./App.css";
-import Login from "./components/views/Login";
-import axios from "axios";
->>>>>>> 352841572fae1c8109d8782914d58d47b4705233
+import Login from "./components/Login";
 
 function App() {
   const [count, setCount] = useState(0);
