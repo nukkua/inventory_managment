25654fee9367b46d22b70186ad17d6d0260f9886
nukkua <marco.salazar.2014.com@gmail.com>
2023-06-06 20:13:35 -0400

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Login } from './components/Login.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import TablaPrecio from "./components/TablaPrecio.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route/>
-        <Route/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/tablaprecio" element={<TablaPrecio />} />
+        <Route />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
