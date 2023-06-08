6a62b492703a53871d1a63774f0b3125c7482884
nukkua <marco.salazar.2014.com@gmail.com>
2023-06-07 21:35:45 -0400

    a

--- src/components/SectionInEscasos.jsx
+++ src/components/SectionInEscasos.jsx
@@ -27,7 +27,7 @@ const SectionInEscasos = () => {
     },
   ];
   return (
-    <TableContainer component={Paper} className="container">
+    <TableContainer style={{ width: '1160px' }}component={Paper} className="table-container">
       <Boton />
       <div className="titulo">Ingredientes escasos</div>
       <Table className="tabla-container">
